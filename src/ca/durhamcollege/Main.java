19dbfd7e41c6b96e1972a6dcc639e6f4ae60f2eb
Gabriel Dietrich <gabrieldietrich@hotmail.com>
2020-11-21 18:37:14 -0500

    Initial Commit

---
+++ src/ca/durhamcollege/Main.java
@@ -0,0 +1,19 @@
+/**
+ * @Authors: Gabriel Dietrich (100764733) and Fleur Blanckaert (100747083
+ * @Date: Nov 21, 2020
+ * @Program: OOP3200 - Java Lab 2
+ * @Description: This application creates a class called WorkTicket, which represents the client's
+ * 				request for IT support.	The class is copied into anew constructor. Later on, the
+ * 				application demonstrates the usability of the operators "==", "=", ">>", and "<<".
+ */
+
+
+package ca.durhamcollege;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/ca/durhamcollege/WorkTicket.java
@@ -0,0 +1,51 @@
+/**
+ * @Authors: Gabriel Dietrich (100764733) and Fleur Blanckaert (100747083
+ * @Date: Nov 21, 2020
+ * @Program: OOP3200 - Java Lab 2
+ * @Description: This application creates a class called WorkTicket, which represents the client's
+ * 				request for IT support.	The class is copied into anew constructor. Later on, the
+ * 				application demonstrates the usability of the operators "==", "=", ">>", and "<<".
+ */
+
+package ca.durhamcollege;
+
+public class WorkTicket
+{
+    // Private instance variables
+    int ticketNumber;
+    String clientId;
+    int ticketDay;
+    int ticketMonth;
+    int ticketYear;
+    String issueDescription;
+
+    // Public properties
+
+    // Accessors
+    public int getTicketNumber() { return ticketNumber;  }
+    public String getClientId() { return clientId; }
+    public int getTicketDay() { return ticketDay; }
+    public int getTicketMonth() { return ticketMonth; }
+    public int getTicketYear() { return ticketYear; }
+    public String getIssueDescription() { return issueDescription; }
+
+    // Accessor Method
+
+    // Mutators
+    public void setTicketNumber(int ticketNumber) { this.ticketNumber = ticketNumber; }
+    public void setClientId(String clientId) { this.clientId = clientId; }
+    public void setTicketDay(int ticketDay) { this.ticketDay = ticketDay; }
+    public void setTicketMonth(int ticketMonth) { this.ticketMonth = ticketMonth; }
+    public void setTicketYear(int ticketYear) { this.ticketYear = ticketYear; }
+    public void setIssueDescription(String issueDescription) { this.issueDescription = issueDescription; }
+
+
+    // Default constructor
+
+
+    // Parameterized constructor
+    WorkTicket(int number, String id, int day, int month, int year, String description)
+    {
+
+    }
+}

0af59978cc5a3a8e0c93a378d42674239656d8ca
Gabriel Dietrich <gabrieldietrich@hotmail.com>
2020-11-22 17:30:34 -0500

    V3

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -14,7 +14,8 @@ import java.time.DateTimeException;
 import java.util.Scanner;
 import java.time.LocalDate;
 
-public class Main {
+public class Main
+{
 
     public static void main(String[] args)
     {
@@ -25,9 +26,10 @@ public class Main {
         String ticketInput; // Has to be int
         String clientInput;
         LocalDate localDate = LocalDate.now();
-        String dayInput; // Has to be LocalDate
-        String monthInput;
-        String yearInput;
+        String workTicketDate;
+        int dayInput; // Has to be LocalDate
+        int monthInput;
+        int yearInput;
         String descriptionInput;
         boolean isValid = false;
 
@@ -50,7 +52,9 @@ public class Main {
             {
                 // Ticker Number
                 System.out.print("The Ticket Number is: ");
+                //ticketInput = ReadInteger(1);
                 ticketInput = keyboard.nextLine(); // ReadInteger
+
 
                 // Client ID
                 do
@@ -67,20 +71,52 @@ public class Main {
 
 
                 // Work Ticket Date
+                System.out.print("The Work Ticket Date is (mm/dd/yyyy): ");
+                //workTicketDate = dateInput();
+                workTicketDate = keyboard.nextLine();
                 System.out.println(localDate);
 
-                // Day
-                System.out.print("The Work Ticket Date is: \nDay: ");
-                dayInput = keyboard.nextLine();
 
-                // Month
-                System.out.print("Month: ");
-                monthInput = keyboard.nextLine();
+                System.out.println("The Work Ticket Date is: ");
+                do
+                {
+                    // Day
+                    System.out.print("Day: ");
+                    dayInput = keyboard.nextInt();
 
-                // Year
-                System.out.print("Year: ");
-                yearInput = keyboard.nextLine();
+                    if(dayInput < MIN_AMOUNT || dayInput > MAX_DAY)
+                    {
+                        System.out.println("\nError: Day must be between " + MIN_AMOUNT + " and " + MAX_DAY + "\n");
+                    }
+                }
+                while(dayInput < MIN_AMOUNT || dayInput > MAX_DAY);
 
+                do
+                {
+                    // Month
+                    System.out.print("Month: ");
+                    monthInput = keyboard.nextInt();
+
+                    if(monthInput < MIN_AMOUNT || monthInput > MAX_MONTH)
+                    {
+                        System.out.println("\nError: Day must be between " + MIN_AMOUNT + " and " + MAX_MONTH + "\n");
+                    }
+                }
+                while(monthInput < MIN_AMOUNT || monthInput > MAX_MONTH);
+
+                do
+                {
+                    // Year
+                    System.out.print("Year: ");
+                    yearInput = keyboard.nextInt();
+
+                    if(yearInput < MIN_YEAR || yearInput > MAX_YEAR)
+                    {
+                        System.out.println("\nError: Day must be between " + MIN_YEAR + " and " + MAX_YEAR + "\n");
+                    }
+
+                }
+                while(yearInput < MIN_YEAR || yearInput > MAX_YEAR);
 
                 // Description
                 do
@@ -90,10 +126,13 @@ public class Main {
 
                     if(descriptionInput.length() < MIN_AMOUNT)
                     {
-                        System.out.println("Description must be at least " + MIN_AMOUNT + " character long");
+                        System.out.println("\nDescription must be at least " + MIN_AMOUNT + " character long");
                     }
                 }
                 while (descriptionInput.length() < MIN_AMOUNT);
+
+                //Sets the WorkTicket
+                //ticket.SetWorkTicket = (ticketInput, clientInput, workTicketDate, descriptionInput);
 
                 // Ends loop
                 isValid = true;
@@ -120,5 +159,13 @@ public class Main {
 
         }
         while(!isValid);
+
+        // Displays the output
+        System.out.println("\nTicket Number\tClient ID\tWork Ticket Date\tIssue Description");
+        //System.out.print(ticket.ShowWorkTicket());
+        //System.out.print(ticketInput + clientInput + workTicketDate + descriptionInput);
+
+
+
     }
 }
--- src/ca/durhamcollege/WorkTicket.java
+++ src/ca/durhamcollege/WorkTicket.java
@@ -9,54 +9,101 @@
 
 package ca.durhamcollege;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class WorkTicket
 {
+
     // Private instance variables
-    int ticketNumber;
-    String clientId;
-    int ticketDay;
-    int ticketMonth;
-    int ticketYear;
-    String issueDescription;
+    private int ticketNumber;
+    private String clientId;
+    private LocalDate ticketDate;
+    //private int ticketDay;
+    //private int ticketMonth;
+    //private int ticketYear;
+    private String issueDescription;
 
     // Public properties
-
-
     // Accessors
     public int getTicketNumber() { return ticketNumber;  }
     public String getClientId() { return clientId; }
-    public int getTicketDay() { return ticketDay; }
-    public int getTicketMonth() { return ticketMonth; }
-    public int getTicketYear() { return ticketYear; }
+    public LocalDate getTicketDate() { return ticketDate; }
+    //public int getTicketDay() { return ticketDay; }
+    //public int getTicketMonth() { return ticketMonth; }
+    //public int getTicketYear() { return ticketYear; }
     public String getIssueDescription() { return issueDescription; }
 
     // Accessor Method
-    //public String ShowWorkTicket();
+    public String ShowWorkTicket()
+    {
+        return null;
+    }
 
     // Mutators
     public void setTicketNumber(int ticketNumber) { this.ticketNumber = ticketNumber; }
     public void setClientId(String clientId) { this.clientId = clientId; }
-    public void setTicketDay(int ticketDay) { this.ticketDay = ticketDay; }
-    public void setTicketMonth(int ticketMonth) { this.ticketMonth = ticketMonth; }
-    public void setTicketYear(int ticketYear) { this.ticketYear = ticketYear; }
+    public void setTicketDate(LocalDate ticketDate) {this.ticketDate = ticketDate; }
+    //public void setTicketDay(int ticketDay) { this.ticketDay = ticketDay; }
+    //public void setTicketMonth(int ticketMonth) { this.ticketMonth = ticketMonth; }
+    //public void setTicketYear(int ticketYear) { this.ticketYear = ticketYear; }
     public void setIssueDescription(String issueDescription) { this.issueDescription = issueDescription; }
 
     // Mutator Method
-    //boolean SetWorkTicket(int number, String id, int day, int month, int year, String description);
+    boolean SetWorkTicket(int number, String id, LocalDate date, String description)
+    {
+        return true;
+    };
 
     // Default constructor
-    //WorkTicket() {ticketNumber(0), clientId(""), ticketDay(1), ticketMonth(1), ticketYear(2000), issueDescription("")};
+    public WorkTicket()
+    {
+        ticketNumber = 0;
+        clientId = null;
+        ticketDate = null;
+        issueDescription = null;
+    }
 
     // Parameterized constructor
-    WorkTicket(int number, String id, int day, int month, int year, String description)
+    WorkTicket(int number, String id, LocalDate date, String description)
     {
         ticketNumber = number;
         clientId = id;
-        ticketDay = day;
-        ticketMonth = month;
-        ticketYear = year;
+        ticketDate = date;
+        //ticketDay = day;
+        //ticketMonth = month;
+        //ticketYear = year;
         issueDescription = description;
     }
 
+    // Returns string that shows object's attributes
+    String ticketString = Integer.toString(ticketNumber);
+    @Override
+    public String toString() { return ticketString + " " + clientId + " " + ticketDate + " " +
+            issueDescription;};
+
+    // Function that reads integer values
+    public int ReadInteger(final int MIN, final int MAX)
+    {
+        double validNumber = 0.0;
+
+        if(validNumber > (int)validNumber)
+        {
+            throw new IllegalArgumentException("Invalid Input. Please enter a whole, positive number\n");
+        }
+
+        return (int)validNumber;
+    }
+
+    // Method to take in date. Accepts string and converts to LocalDate
+    //https://stackoverflow.com/questions/42522744/taking-input-using-localdate
+    public static LocalDate dateInput(String userInput)
+    {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("mm/dd/yyyy");
+        LocalDate ticketDate = LocalDate.parse(userInput, dateFormat);
+
+        System.out.println(ticketDate);
+        return ticketDate;
+    }
 
 }
